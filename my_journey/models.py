b54edd9e333be26841eefab3768e411b5c007e3c
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-10-12 14:13:29 -0400

    Changed the models

--- my_journey/models.py
+++ my_journey/models.py
@@ -3,17 +3,19 @@ from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, null=False)
+    password = models.CharField(max_length=200, null=False)
+    friends = ArrayField(models.TextField(), blank=True)
 
     def __str__(self):
         return self.username
 
 class Picture(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
-    image_url = models.TextField()
-    likes = models.BigIntegerField()
-    dislikes = models.BigIntegerField()
-    comments = ArrayField(models.TextField())
+    image_url = models.TextField(null=False)
+    likes = models.BigIntegerField(default=0)
+    dislikes = models.BigIntegerField(default=0)
+    comments = ArrayField(models.TextField(), blank=True)
 
     def __str__(self):
         return self.image_url
--- my_journey/views.py
+++ my_journey/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+# Importing models
+from .models import User, Picture
 
 # Create your views here.
