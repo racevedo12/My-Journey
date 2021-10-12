6765f2c9f23248838a988f02964f31e79647f27b
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-10-12 11:15:29 -0400

    Setting up database

--- my_journey_project/settings.py
+++ my_journey_project/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'my_journey_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_journey',
+        'USER': 'my_journey_user',
+        'PASSWORD': 'my_journey',
+        'HOST': 'localhost',
     }
 }
 
---
+++ settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE my_journey;
+CREATE USER my_journey_user WITH PASSWORD 'my_journey';
+GRANT ALL PRIVILEGES ON DATABASE my_journey TO my_journey_user;
