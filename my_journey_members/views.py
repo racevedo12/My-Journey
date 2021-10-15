ab955eafcffef640a82fca2cf724a85fbb9c4980
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-10-15 10:26:36 -0400

    :bulb: Adding comments to add_picture.html and views.py for the my_journey app

--- my_journey_members/views.py
+++ my_journey_members/views.py
@@ -9,4 +9,7 @@ from django.urls import reverse_lazy
 class UserRegistrationView(generic.CreateView):
     form_class = UserCreationForm
     template_name = "registration/register.html"
+
+    # Using reverse_lazy to provide a reverse url(redirect) to the login template that django has built in
+    # https://docs.djangoproject.com/en/3.2/ref/urlresolvers/
     success_url = reverse_lazy("login")
