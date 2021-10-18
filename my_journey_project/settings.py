3ec3ca199d12ec960be9040cee75c2a2ae08aedd
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-10-18 15:29:35 -0400

    :construction: Configuring AWS Still

--- my_journey_project/settings.py
+++ my_journey_project/settings.py
@@ -137,8 +137,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "my_journey", "static")
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), "../my_journey/", 'static')
+STATIC_URL = 'static'
 
 
 # Default primary key field type
