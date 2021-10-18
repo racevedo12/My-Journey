8ef4c14e53529d74dee6ab4e72cf88e4448acf67
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-10-18 14:15:29 -0400

    Modified Static root for static files

--- my_journey_project/settings.py
+++ my_journey_project/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "my_journey/static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
